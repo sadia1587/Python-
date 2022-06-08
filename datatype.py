4544c3494693d0318978c49de886e682a40d0433
sadia <sadia.afrous@diu.edu.bd>
2022-06-07 21:28:05 -0400

    usining function

--- datatype.py
+++ datatype.py
@@ -7,3 +7,17 @@ print(type(z))
 print(x)
 print(y)
 print(z)
+
+#namefunc.py
+Name1 = "tulu"
+age1 = "1.1"
+bod1 = "2021"
+
+def printinfo(Name, age, bod):
+    print(Name + age + bod)
+    
+    
+printinfo(Name1, age1, bod1) 
+   
+
+
