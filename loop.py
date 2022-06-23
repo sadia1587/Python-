cc9de8a8b0faeaae5afccb5fdca9c9280487310e
sadia <sadia.afrous@diu.edu.bd>
2022-06-22 20:57:56 -0400

    List usingloop

---
+++ loop.py
@@ -0,0 +1,22 @@
+x = [1,2,3,4,5,6,7,8,9,10]
+# using while loop
+i= len(x)-1
+print(i)
+while 0 < i:
+    print(f"jun 2022 Day: {(x[i])} ")
+    i =i-1
+
+
+print("==========================================")
+
+# using For loop
+
+for i in range(len(x) -1,-1, -1):
+    print(f'july 2022 Day: {x[i]}')
+    i = i - 1
+
+''''
+#using loop
+for j in range(len(x)):
+    print (x[j].reversed(x))
+'''''
